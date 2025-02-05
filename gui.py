4af800ff5a2350b6f586d5fd72225997a70cee90
Ryan Lima <141368391+Ryan3Lima@users.noreply.github.com>
2025-02-05 11:44:01 -0700

    changed main.py to cli.py, started creating gui.py

--- gui.py
+++ gui.py
@@ -1,15 +1,43 @@
 import functions
 import FreeSimpleGUI as sg
 
-label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter Todo")
-add_button = sg.Button("Add")
-exit_button = sg.Button("Exit")
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+from functions import get_todos
 
-window = sg.Window('My To-Do App', layout=[[label],[input_box, add_button], [edit_button, complete_button],[exit_button]]) # Window Title
-window.read()
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="_Enter Todo_", key='todo')
+add_button = sg.Button("Add")
+exit_button = sg.Button("Exit") # non functional
+edit_button = sg.Button("Edit") # non functional
+complete_button = sg.Button("Complete") # non functional
+
+
+window = sg.Window('My To-Do App',
+                   layout=[[label],
+                           [input_box, add_button],
+                           [edit_button, complete_button],
+                           [exit_button]],
+                   font=('Helvetica',12))
+
+while True:
+    event, values = window.read()
+
+    match event:
+        case "Add":
+           todos = functions.get_todos()
+           new_todo = values['todo'] + "\n"
+           todos.append(new_todo)
+           functions.write_todos(todos)
+
+        case "Edit":
+            print(values)
+        case "Complete":
+            print(values)
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
 
 
